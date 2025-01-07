1c593919e0b501ca662baa6b1bcc3f697a74484e
Anshik01 <anshik0706@gmail.com>
2025-01-07 20:36:14 +0530

    Kilometer to mile Converter program added

---
+++ DistanceConverter.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DistanceConverter{
+		public static void main(String[]args){
+			Scanner sc = new Scanner(System.in);
+			
+			double distance = sc.nextDouble();
+			
+			System.out.println(distance * 0.621371);
+		}
+}
